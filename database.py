8e8cb2f1a9e4382214023e1a969fb4be5124d440
YISR-UOH <yerko.sepulveda@pregrado.uoh.cl>
2022-10-20 19:49:20 -0400

    .

--- database.py
+++ database.py
@@ -1,12 +1,12 @@
 from pymongo import MongoClient
-import certifi
+
 
 MONGO_URI = 'mongodb+srv://vEzzel:v3zzel_Web_Company@vezzel.lgiwpov.mongodb.net/?retryWrites=true&w=majority'
-ca = certifi.where()
+
 
 def dbConnection():
     try:
-        client = MongoClient(MONGO_URI, tlsCAFile=ca)
+        client = MongoClient(MONGO_URI)
         db = client.vEzzel
     except ConnectionError:
         print('Error de conexión con la bdd')
