aee2d9b6828d6299e6c1f8cbbc0227771a6a1d0d
YISR-UOH <yerko.sepulveda@pregrado.uoh.cl>
2022-11-04 15:21:39 -0300

    Update db.py

--- db.py
+++ db.py
@@ -1,12 +1,12 @@
 from pymongo import MongoClient
-
+import os
 
 
 def dbconection():
   #mongodb+srv://vEzzel:v3zzel_Web_Company@vezzel.lgiwpov.mongodb.net/?retryWrites=true&w=majority
 
   #os.getenv("MONGODB_URI")
-  MONGO_URI = 'mongodb+srv://vEzzel:v3zzel_Web_Company@vezzel.lgiwpov.mongodb.net/?retryWrites=true&w=majority'
+  MONGO_URI = os.getenv("MONGODB_URI")
 
   client = MongoClient(MONGO_URI)
   db = client.vEzzel
